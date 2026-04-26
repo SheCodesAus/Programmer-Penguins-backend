eb1d73fb49e30c8ffb744b64d94911e08a34662e
Alist <alistratovame@outlook.com>
2026-04-26 21:53:16 +1000

    Add username to user serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -11,6 +11,7 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = [
             "id",
+            "username",
             "email",
             "first_name",
             "last_name",
